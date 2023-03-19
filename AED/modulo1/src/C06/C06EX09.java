3ef6ecf5bc4fea0ca4e3d5f257f17c76239024f7
Marcello Cavazza <marcellocavazzaoliveira.dev@gmail.com>
2023-03-19 12:43:04 -0300

    feito ex 9 c06

--- AED/modulo1/src/C06/C06EX09.java
+++ AED/modulo1/src/C06/C06EX09.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 
 public class C06EX09 {
     public static void main(String[] args) {
-        char genero;
         Scanner teclado = new Scanner(System.in);
 
-        System.out.print("Digite a letra do seu genero:"+
-                "\n   'F' para feminino;"+
-                "\n   'M' para masculino;");
-        genero = teclado.next().charAt(0);
-        if(String.valueOf(genero) == ""){
-
+        System.out.print("'F' para feminino;\n"+
+                "'M' para masculino\n"+
+                "Digite a letra do seu genero:");
+        char genero = teclado.next().charAt(0);
+        if(genero == 'M' || genero == 'm' || genero == 'F' || genero == 'f'){
+            double pesoIdeal = 0;
+            System.out.print("Qual a sua altura?");
+            double altura = teclado.nextDouble();
+            if(genero == 'M' || genero == 'm'){
+                pesoIdeal = 72.7 * altura - 58;
+            }else if (genero == 'F' || genero == 'f'){
+                pesoIdeal = 62.1 * altura - 44.7;
+            }
+            System.out.println("Peso do usuario: "+ String.format("%.3f",pesoIdeal));
+        }else{
+            System.out.println("Fechando programa...\n"+
+                    "Somente selecione M ou F");
         }
     }
 }
