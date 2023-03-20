68e3548cf17b61e518fda4bfcaf567304e1f6281
Marcello Cavazza <marcellocavazzaoliveira.dev@gmail.com>
2023-03-20 19:04:55 -0300

    falta somente o 15

--- AED/modulo1/src/C06/C06EX11.java
+++ AED/modulo1/src/C06/C06EX11.java
@@ -19,28 +19,62 @@ public class C06EX11 {
         System.out.print("Sets da Equipe 2: ");
         int equipe2SetsGanhos = teclado.nextInt();
 
-        if(equipe1SetsGanhos == 0){
-            mensagemFinal = "Resultado:\n"+
-                    "Equipe 1 = "+equipe1Nome+
-                    "\nSets da equipe 1 = 0"+
-                    "\nPontos "+equipe1Nome+" = 0"+
-                    "\nEquipe 2 = "+equipe2Nome+
-                    "\nSets da equipe 2 = 3"+
-                    "\nPontos "+equipe2Nome+" = 3";
-        }else if(equipe2SetsGanhos == 0){
-            mensagemFinal = "Resultado:\n"+
-                    "Equipe 1 = "+equipe1Nome+
-                    "\nSets da equipe 1 = 3"+
-                    "\nPontos "+equipe1Nome+" = 3"+
-                    "\nEquipe 2 = "+equipe2Nome+
-                    "\nSets da equipe 2 = 0"+
-                    "\nPontos "+equipe2Nome+" = 0";
-        }else{
+        if (equipe1SetsGanhos == 0) {
+            mensagemFinal = "Resultado:\n" +
+                    "Equipe 1 = " + equipe1Nome +
+                    "\nSets da equipe 1 = 0" +
+                    "\nPontos " + equipe1Nome + " = 0" +
+                    "\nEquipe 2 = " + equipe2Nome +
+                    "\nSets da equipe 2 = 3" +
+                    "\nPontos " + equipe2Nome + " = 3";
+        } else if (equipe2SetsGanhos == 0) {
+            mensagemFinal = "Resultado:\n" +
+                    "Equipe 1 = " + equipe1Nome +
+                    "\nSets da equipe 1 = 3" +
+                    "\nPontos " + equipe1Nome + " = 3" +
+                    "\nEquipe 2 = " + equipe2Nome +
+                    "\nSets da equipe 2 = 0" +
+                    "\nPontos " + equipe2Nome + " = 0";
+        } else {
+            if (equipe1SetsGanhos == 3 && equipe2SetsGanhos == 1) {
+                mensagemFinal = "Resultado:\n" +
+                        "Equipe 1 = " + equipe1Nome +
+                        "\nSets da equipe 1 = 3" +
+                        "\nPontos " + equipe1Nome + " = 3" +
+                        "\nEquipe 2 = " + equipe2Nome +
+                        "\nSets da equipe 2 = 1" +
+                        "\nPontos " + equipe2Nome + " = 0";
+            } else if (equipe1SetsGanhos == 1 && equipe2SetsGanhos == 3) {
+                mensagemFinal = "Resultado:\n" +
+                        "Equipe 1 = " + equipe1Nome +
+                        "\nSets da equipe 1 = 1" +
+                        "\nPontos " + equipe1Nome + " = 0" +
+                        "\nEquipe 2 = " + equipe2Nome +
+                        "\nSets da equipe 2 = 3" +
+                        "\nPontos " + equipe2Nome + " = 3";
+            } else {
+                if (equipe1SetsGanhos == 3 && equipe2SetsGanhos == 2) {
+                    mensagemFinal = "Resultado:\n" +
+                            "Equipe 1 = " + equipe1Nome +
+                            "\nSets da equipe 1 = 3" +
+                            "\nPontos " + equipe1Nome + " = 3" +
+                            "\nEquipe 2 = " + equipe2Nome +
+                            "\nSets da equipe 2 = 2" +
+                            "\nPontos " + equipe2Nome + " = 1";
+                } else if (equipe1SetsGanhos == 2 && equipe2SetsGanhos == 3) {
+                    mensagemFinal = "Resultado:\n" +
+                            "Equipe 1 = " + equipe1Nome +
+                            "\nSets da equipe 1 = 2" +
+                            "\nPontos " + equipe1Nome + " = 1" +
+                            "\nEquipe 2 = " + equipe2Nome +
+                            "\nSets da equipe 2 = 3" +
+                            "\nPontos " + equipe2Nome + " = 3";
+                }
+            }
+
 
         }
-
         System.out.println(mensagemFinal);
-
 
         teclado.close();
     }
