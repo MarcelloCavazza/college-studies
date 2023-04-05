3c5abbea72f395520224186c66f3d0ae1c72259e
Marcello Cavazza <marcellocavazzaoliveira.dev@gmail.com>
2023-04-04 21:45:37 -0300

    Add files via upload

---
+++ AED/modulo_II/src/C08/C08EX06.java
@@ -0,0 +1,36 @@
+package C08;
+
+import java.util.Scanner;
+
+public class C08EX06 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numeroInseridoPeloUsuario, somaDeNumerosDivisiveisPor4 = 0;
+        byte quantidadeDeNumerosDivisiveisPor3 = 0;
+
+        for (byte i = 0; i < 10; i++){
+            System.out.print("Insira um numero inteiro: ");
+            numeroInseridoPeloUsuario = teclado.nextInt();
+            final boolean ehPar = numeroInseridoPeloUsuario % 2 == 0;
+            if(ehPar){
+                System.out.println("O numero "+numeroInseridoPeloUsuario+" eh par!");
+            }else{
+                System.out.println("O numero "+numeroInseridoPeloUsuario+" eh impar!");
+            }
+
+            final boolean ehDivisivelPor4 = numeroInseridoPeloUsuario % 4 == 0;
+            if(ehDivisivelPor4){
+                somaDeNumerosDivisiveisPor4 += numeroInseridoPeloUsuario;
+            }
+            final boolean ehDivisivelPor3 = numeroInseridoPeloUsuario % 3 == 0;
+            if(ehDivisivelPor3){
+                quantidadeDeNumerosDivisiveisPor3++;
+            }
+        }
+
+        System.out.println("Soma dos numeros divisiveis por 4 = "+somaDeNumerosDivisiveisPor4);
+        System.out.println("Quantidade de numeros divisiveis por 3 = "+quantidadeDeNumerosDivisiveisPor3);
+
+        teclado.close();
+    }
+}
