4c0146154a941f1094fe3cf314a9d1464d37164b
Marcello Cavazza <marcellocavazzaoliveira.dev@gmail.com>
2023-04-11 21:54:34 -0300

    C08EX07

---
+++ AED/modulo_II/src/C08/C08EX07.java
@@ -0,0 +1,33 @@
+package C08;
+
+import java.util.Scanner;
+
+public class C08EX07 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        int idade, pessoasComAte12Anos = 0, pessoasComMaisDe30Anos = 0;
+        double media = 0, aux = 5.0;
+        for (int i = 0; i < aux; i++) {
+            System.out.print("Insira seu nome: ");
+            teclado.nextLine();
+            if(i > 0){
+                teclado.nextLine();
+            }
+            System.out.print("Insira sua idade: ");
+            idade = teclado.nextInt();
+            if(idade <=12){
+                pessoasComAte12Anos++;
+            }
+            if(idade > 30){
+                pessoasComMaisDe30Anos++;
+            }
+           media += idade; 
+        }
+        media = media / aux;
+        System.out.println("Pessoas com ate 12 anos: "+pessoasComAte12Anos);
+        System.out.println("Pessoas com mais de 30 anos: "+pessoasComMaisDe30Anos); 
+        System.out.println("Media total das idades inseridas:"+media); 
+        teclado.close();
+    }
+}
