8031393c6c9d7a438c91ad82dd74acd5a3a8df40
Marcello Cavazza <marcellocavazzaoliveira.dev@gmail.com>
2023-04-17 20:07:34 -0300

    13 certa

--- AED/modulo_II/src/C08/C08EX13.java
+++ AED/modulo_II/src/C08/C08EX13.java
@@ -1,56 +1,55 @@
 package C08;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class C08EX13 {
-    public static void main(String[] args) {
-        String arquivo = "cidades.txt";
-        int contadorPopulacaoMaior1Milhao = 0;
-        int contadorMulheresMaisQueHomens = 0;
-        int menorPopulacao = Integer.MAX_VALUE;
-        String nomeCidadeMenorPopulacao = "";
-        
-        try {
-            BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
-            String linha;
-            while ((linha = leitor.readLine()) != null) {
-                String[] dados = linha.split(";");
-                String nomeCidade = dados[0];
-                int populacao = Integer.parseInt(dados[1]);
-                int eleitores = Integer.parseInt(dados[2]);
-                int mulheres = Integer.parseInt(dados[3]);
-                int homens = Integer.parseInt(dados[4]);
-                
-                if (homens + mulheres != populacao) {
-                    System.out.println("A soma de homens e mulheres de " + nomeCidade + " difere da população!");
-                }
-                
-                double percentualEleitores = (double) eleitores / populacao * 100;
-                System.out.println("Percentual de eleitores em " + nomeCidade + ": " + percentualEleitores + "%");
-                
-                if (populacao > 1000000) {
-                    contadorPopulacaoMaior1Milhao++;
-                }
-                
-                if (mulheres > homens) {
-                    contadorMulheresMaisQueHomens++;
-                }
-                
-                if (populacao < menorPopulacao) {
-                    menorPopulacao = populacao;
-                    nomeCidadeMenorPopulacao = nomeCidade;
+    public static void main(String[] args) throws IOException {
+        Scanner arquivo = new Scanner(C08EX13.class.getResourceAsStream("c08ex13.txt"));
+        String linhaDoArquivo = "", nomeCidade = "", menorCidade = "";
+        double porcentagemDePessoasQueVotam;
+        int totalHomens = 0, totalmulheres = 0, populacao = 0,
+                quantidadeEleitores = 0, acimaDe1Milhao = 0, maioriaMulher = 0, menorPop = Integer.MAX_VALUE;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                linhaDoArquivo = arquivo.nextLine();
+                switch (j+1){
+                    case 1:
+                        nomeCidade = linhaDoArquivo;
+                        break;
+                    case 2:
+                        populacao = Integer.parseInt(linhaDoArquivo.replaceAll("\\.",""));
+                        break;
+                    case 3:
+                        quantidadeEleitores = Integer.parseInt(linhaDoArquivo.replaceAll("\\.",""));
+                        break;
+                    case 4:
+                        totalHomens = Integer.parseInt(linhaDoArquivo.replaceAll("\\.",""));
+                        break;
+                    default:
+                        totalmulheres =  Integer.parseInt(linhaDoArquivo.replaceAll("\\.",""));
+                        break;
                 }
             }
-            leitor.close();
-        } catch (IOException e) {
-            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+            boolean populacaoEhCoeza = (totalHomens + totalmulheres) != populacao;
+            if(populacaoEhCoeza) System.out.println(
+                    nomeCidade+": O total de homens e mulheres juntons diferem da populacao total!"
+            );
+            porcentagemDePessoasQueVotam = (quantidadeEleitores *100.0) / populacao;
+            System.out.println(nomeCidade+": Porcentagem de pessoas que votam: "+String.format("%.0f",porcentagemDePessoasQueVotam)+"%");
+            if(populacao > 1000000 && populacaoEhCoeza) acimaDe1Milhao++;
+            if(totalmulheres > totalHomens && populacaoEhCoeza) maioriaMulher++;
+            if(populacao < menorPop) {
+                menorPop = populacao;
+                menorCidade = nomeCidade;
+            };
         }
-        
-        System.out.println("Quantidade de cidades com população acima de 1 milhão de habitantes: " + contadorPopulacaoMaior1Milhao);
-        System.out.println("Quantidade de cidades cuja população tem mais mulheres: " + contadorMulheresMaisQueHomens);
-        System.out.println("Nome da cidade com menor população: " + nomeCidadeMenorPopulacao);
+        System.out.println("-----------------------------------------------------\n"
+        +"-----------------------------------------------------");
+        System.out.println("Quantidade de cidades com mais de 1 milhao de pessoas: "+acimaDe1Milhao);
+        System.out.println("Quantidade de cidades com maioria mulheres: "+maioriaMulher);
+        System.out.println("Cidade com menor populacao "+menorCidade);
+        arquivo.close();
     }
 }
 
