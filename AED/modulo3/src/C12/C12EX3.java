c08adf3bfca20b8309a8c451e5eac375597d0a8a
MarcelloCavazza <marcellocavazzaoliveira.dev@gmail.com>
2023-05-18 21:00:47 -0300

    feito 3

--- AED/modulo3/src/C12/C12EX3.java
+++ AED/modulo3/src/C12/C12EX3.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class C12EX3 {
 	public static void main(String[] args){
+		int[] numeros = new int[10];
+		Scanner teclado = new Scanner(System.in);
 
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print("Insira um numero: ");
+			numeros[i] = teclado.nextInt();
+		}
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println(Math.pow(numeros[i], 2));
+		}
+
+		teclado.close();
 	}
 }
